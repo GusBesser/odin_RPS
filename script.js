d1bcabb859fa060866508a56cc112a698c6d2b10
Gustavo Esser <esser.gustavo@gmail.com>
2025-06-02 16:36:30 -0300

    Create and test getComputerChoice function
    This function generates a random number between 1 and 3 and returns
    either Rock, Paper or Scissors depending on the number

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+
+    choice = Math.floor(Math.random() * 3 + 1)
+    
+    if (choice === 1) {
+        return "Rock"
+    }
+    if (choice === 2) {
+        return "Paper"
+    }
+    if (choice === 3) {
+        return "Scissor"
+    }
+}
+
+console.log(getComputerChoice());

563c571d2e49b8cfad767c3fb76c497857fe56bf
Gustavo Esser <esser.gustavo@gmail.com>
2025-06-02 16:44:15 -0300

    Create getPlayerChoice function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Hello World!");
 function getComputerChoice() {
 
     choice = Math.floor(Math.random() * 3 + 1)
-    
+
     if (choice === 1) {
         return "Rock"
     }
@@ -15,4 +15,11 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    choice = prompt('Type "Rock", "Paper" or "Scissors": ')
+    return choice
+}
+
+console.log(playerChoice);
+console.log(computerChoice);
+
